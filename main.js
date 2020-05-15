68f8de3ed8a6af8759812a3556428b572718f276
Tasha Stenner <tashas1997@gmail.com>
2020-05-15 09:51:34 +0100

    bug-free

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const colors = [
     `pink`, 
     `green`,
     `purple`,
-    `red`
+    `red`  
 ];
 
 // The Tetrominoes
@@ -172,23 +172,22 @@ startBtn.addEventListener('click' , () => {
 })
 
 const addScore = () => {
-    for(let i = 0; i < 199; i +=width) {
-        const row = [i, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9];
-        
+    for (let i = 0; i < 199; i +=width) {
+        const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+  
         if(row.every(index => squares[index].classList.contains(`taken`))) {
-            score += 10
-            scoreDisplay.innerHTML = score
-            row.forEach(index => {
-                squares[index].classList.remove(`taken`)
-                squares[index].classList.remove(`tetromino`)
-                squares[index].style.backgroundColor = ''
-            })
-            
-            const squaresRemoved = squares.splice(i, width)
-            squares = squaresRemoved.concat(squares)
-            squares.forEach(cell => grid.appendChild(cell))
+          score +=10
+          scoreDisplay.innerHTML = score
+          row.forEach(index => {
+            squares[index].classList.remove(`taken`)
+            squares[index].classList.remove(`tetromino`)
+            squares[index].style.backgroundColor = ''
+          })
+          const squaresRemoved = squares.splice(i, width)
+          squares = squaresRemoved.concat(squares)
+          squares.forEach(cell => grid.appendChild(cell))
         }
-    }
+      }
 }
 
 const gameOver = () => {
